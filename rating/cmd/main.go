a50661ef45bd4ac326036968ef662a2b685b5b29
Haile <hailecst@gmail.com>
2026-03-21 18:44:09 +0300

    main file added for rating service

---
+++ rating/cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/high-la/movieapp/rating/internal/controller/rating"
+	httphandler "github.com/high-la/movieapp/rating/internal/handler/http"
+	"github.com/high-la/movieapp/rating/internal/repository/memory"
+)
+
+func main() {
+
+	log.Println("Starting the rating service")
+
+	repo := memory.New()
+	ctrl := rating.New(repo)
+	h := httphandler.New(ctrl)
+
+	http.Handle("/rating", http.HandlerFunc(h.Handle))
+	if err := http.ListenAndServe(":8082", nil); err != nil {
+		panic(err)
+	}
+}
