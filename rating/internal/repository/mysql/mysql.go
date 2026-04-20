cea40c0198f6bdfbeec5626b1c0ef4420c8388e6
Haile <hailecst@gmail.com>
2026-04-20 16:24:30 +0300

    read and write implementation to rating service mysql repo

--- rating/internal/repository/mysql/mysql.go
+++ rating/internal/repository/mysql/mysql.go
@@ -1,6 +1,11 @@
 package mysql
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+
+	"github.com/high-la/movieapp/rating/pkg/model"
+)
 
 // Repository defines a MySQL-based rating repository.
 type Repository struct {
@@ -14,4 +19,33 @@ func New() (*Repository, error) {
 		return nil, err
 	}
 	return &Repository{db}, nil
+}
+
+// Get retrieves all ratings for a given record.
+func (r *Repository) Get(ctx context.Context, recordID model.RecordID, recordType model.RecordType) ([]model.Rating, error) {
+	rows, err := r.db.QueryContext(ctx, "SELECT user_id, value FROM ratings WHERE record_id = ? AND record_type = ?", recordID, recordType)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var res []model.Rating
+	for rows.Next() {
+		var userID string
+		var value int32
+		if err := rows.Scan(&userID, &value); err != nil {
+			return nil, err
+		}
+		res = append(res, model.Rating{
+			UserID: model.UserID(userID),
+			Value:  model.RatingValue(value),
+		})
+	}
+	return res, nil
+}
+
+// Put adds a rating for a given record.
+func (r *Repository) Put(ctx context.Context, recordID model.RecordID, recordType model.RecordType, rating *model.Rating) error {
+	_, err := r.db.ExecContext(ctx, "INSERT INTO ratings (record_id, record_type, user_id, value) VALUES (?, ?, ?, ?)",
+		recordID, recordType, rating.UserID, rating.Value)
+	return err
 }
